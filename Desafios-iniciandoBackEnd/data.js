e5d815101f8bc74ea7ccda33ded594593571fe38
unknown <thyall96@gmail.com>
2020-07-28 16:58:46 -0300

    dados dinamicos index

---
+++ Desafios-iniciandoBackEnd/data.js
@@ -0,0 +1,43 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário animado com JS puro e CSS",
+        duration: "57 min",
+        price: "Free"
+    },
+
+    {
+        id: "2alg7MQ6_sI",
+        title: "GIT e GITHUB na prática gui para iniciantes",
+        duration: "33 min",
+        price: "Free"
+    },
+
+    {
+        id: "ghTrp1x_1As",
+        title: "O que é API? Rest e Restful? | Mayk Brito",
+        duration: "33 min",
+        price: "Free"
+    },
+
+    {
+        id: "AVNADGzXrrQ",
+        title: "Ambiente de desenvolvimento de NODEJS com Docker",
+        duration: "17 min",
+        price: "Free"
+    },
+
+    {
+        id: "rAzHvYnQ8DY",
+        title: "ARRAY: HIGH ORDER FUNCTIONS | Mayk Brito",
+        duration: "54 min",
+        price: "Free"
+    },
+
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável THIS | Mayk Brito",
+        duration: "34 min",
+        price: "Free"
+    }
+]
--- Desafios-iniciandoBackEnd/server.js
+++ Desafios-iniciandoBackEnd/server.js
@@ -2,21 +2,35 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res) {
-    return res.render("about")
+
+    const about = {
+        avatar_url: "https://avatars3.githubusercontent.com/u/29062173?s=460&u=eb568d24153e4804195322bcc765de1236752c42&v=4",
+        name: "Thyall D'greville",
+        role: "Estudante de TI",
+        description: 'Estudante de programação e atualmente realizando o curso de LaunchBase da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links: [
+            { name: "GitHub", url: "https://github.com/thyall"},
+            { name: "Twitter", url: "https://twitter.com/Thyall1/"},
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/thyall-dgreville-651889195/"}
+        ]
+    }
+    return res.render("about", { about })
 })
 
 server.get("/portfolio", function(req, res) {
-    return res.render("portfolio")
+    return res.render("portfolio", { items: videos })
 })
 
 server.listen(5000, function() {
