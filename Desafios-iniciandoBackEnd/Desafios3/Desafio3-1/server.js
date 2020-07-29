55568cc933af816994cf140ebc4f2a42e8354aef
unknown <thyall96@gmail.com>
2020-07-29 19:50:00 -0300

    desafio 3-3

--- Desafios-iniciandoBackEnd/Desafios3/Desafio3-1/server.js
+++ Desafios-iniciandoBackEnd/Desafios3/Desafio3-1/server.js
@@ -10,7 +10,8 @@ server.use(express.static('public'))
 
 nunjucks.configure("views", {
     express: server,
-    autoescape: false
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function(req, res) {
@@ -25,6 +26,22 @@ server.get("/", function(req, res) {
 server.get("/courses", function(req, res) {
     return res.render("courses", {items: cursos})
 })
+
+server.get("/course/:id", function(req, res) {
+    const id = req.params.id;
+
+    const curso = cursos.find(function(curso){
+        if(curso.id == id){
+            return true
+        }
+    })
+
+    if(!curso){
+        return res.send('NOT FOUND')
+    }
+
+    return res.render("course", { item: curso })
+  });
 
 server.use(function(req, res) {
     res.status(404).render("not-found");
